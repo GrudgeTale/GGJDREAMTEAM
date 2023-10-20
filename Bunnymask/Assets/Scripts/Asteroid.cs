c0f47233b36df4272d9af0e3e9ab73606f5e22ad
BagelstohHehe <ctpaglinawan@gmail.com>
2023-10-21 02:29:43 +0800

    Asteroid Update 1

--- Bunnymask/Assets/Scripts/Asteroid.cs
+++ Bunnymask/Assets/Scripts/Asteroid.cs
@@ -8,9 +8,13 @@ public class Asteroid : MonoBehaviour
 
     public float size = 1.0f;
 
-    public float winSize = 0.5f;
+    public float minSize = 0.5f;
 
     public float maxSize = 1.5f;
+
+    public float speed = 50.0f;
+
+    public float maxLifetime = 30.0f;
 
     private SpriteRenderer _spriteRenderer;
 
@@ -34,8 +38,11 @@ public class Asteroid : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void SetTrajectory(Vector2 direction)
     {
-        
+        _rigidbody.AddForce(direction * this.speed);
+         
+        Destroy(this.gameObject, this.maxLifetime);
     }
+
 }
