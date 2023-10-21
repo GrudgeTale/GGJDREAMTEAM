e21b3a3ab2dd3a19f3c1aac747787a78f952d6a6
GrudgeTale <89556321+GrudgeTale@users.noreply.github.com>
2023-10-21 18:05:21 +0800

    Ammo System

--- Bunnymask/Assets/Scripts/Player/PlayerController.cs
+++ Bunnymask/Assets/Scripts/Player/PlayerController.cs
@@ -33,28 +33,12 @@ public class PlayerController : MonoBehaviour
         Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
 
         transform.up = direction;
-        // Moving After each click of mouse button
-        if(Input.GetMouseButtonDown(0)){
-            // Generate bullets
-            if(Time.time-lastShot < cooldown){
-                return;
-            }
-            lastShot = Time.time;
-
-            
-            GameObject newBullet = Instantiate(bullet, transform.position, transform.rotation);
-            newBullet.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * bulletSpeed);
-            Destroy(newBullet, 4f);
-            
-         //   DestroyBullet bullet = Instantiate(this.bulletPrefab, this.transform.position, this.transform.rotation);
-         //  bullet.Project(this.transform.up);
-            player.AddForce(-transform.up *strength, ForceMode2D.Impulse);
-            
-        }
 
         
 
-        
+
+
+
         /* Screen Warp
         Vector2 newPos = transform.position;
         if(transform.position.y > screenTop){
@@ -76,14 +60,20 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Asteroid")
+        if (collision.gameObject.CompareTag("Asteroid"))
         {
             player.velocity = Vector3.zero;
             player.angularVelocity = 0.0f;
+
 
             this.gameObject.SetActive(false);
 
             FindObjectOfType<GameManager>().PlayerDied();
         }
+    }
+
+    public void Knockback()
+    {
+        player.AddForce(-transform.up * strength, ForceMode2D.Impulse);
     }
 }
