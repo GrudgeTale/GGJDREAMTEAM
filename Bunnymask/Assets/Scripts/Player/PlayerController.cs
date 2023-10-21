f0acf8b5b9963f858b359efe2351442dfa8f4fd0
earth <89556321+GrudgeTale@users.noreply.github.com>
2023-10-21 21:25:29 +0800

    g

--- Bunnymask/Assets/Scripts/Player/PlayerController.cs
+++ Bunnymask/Assets/Scripts/Player/PlayerController.cs
@@ -20,7 +20,6 @@ public class PlayerController : MonoBehaviour
     public float bulletSpeed;
     public float cooldown;
     float lastShot;
-    public Ammo current;
 
  
 
@@ -37,12 +36,28 @@ public class PlayerController : MonoBehaviour
         Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
 
         transform.up = direction;
+        // Moving After each click of mouse button
+        if(Input.GetMouseButtonDown(0)){
+            if(Time.time-lastShot < cooldown){
+                return;
+            }
+            lastShot = Time.time;
+
+            
+            GameObject newBullet = Instantiate(bullet, transform.position, transform.rotation);
+            newBullet.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * bulletSpeed);
+            Destroy(newBullet, 4f);
+            
+         //   DestroyBullet bullet = Instantiate(this.bulletPrefab, this.transform.position, this.transform.rotation);
+         //  bullet.Project(this.transform.up);
+
+            player.AddForce(-transform.up *strength, ForceMode2D.Impulse);
+            
+        }
 
         
 
-
-
-
+        
         /* Screen Warp
         Vector2 newPos = transform.position;
         if(transform.position.y > screenTop){
@@ -64,20 +79,14 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Asteroid"))
+        if (collision.gameObject.tag == "Asteroid")
         {
             player.velocity = Vector3.zero;
             player.angularVelocity = 0.0f;
-            current.CurrentAmmo = 6;
 
             this.gameObject.SetActive(false);
 
             FindObjectOfType<GameManager>().PlayerDied();
         }
-    }
-
-    public void Knockback()
-    {
-        player.AddForce(-transform.up * strength, ForceMode2D.Impulse);
     }
 }
